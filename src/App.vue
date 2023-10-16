d341a11bc45d0d981f00472666a4456d7ca4a0da
Bruno Chaves <chavesbruno699@gmail.com>
2023-10-16 15:21:34 -0300

    Reusable-skeleton-loader

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <script setup>
+import { Suspense } from 'vue';
+import UserCard from './components/UserCard.vue';
+import UserCardSkeletonVue from './components/UserCardSkeleton.vue';
+
 
 </script>
 
 <template>
-  <div>
-    <h1>Hello Vue 3</h1>
-  </div>
+  <main>
+    <Suspense>
+      <UserCard />
+      <template #fallback>
+        <UserCardSkeletonVue />
+      </template>
+    </Suspense>
+  </main>
 </template>
 
-<style scoped>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Mouse+Memoirs&display=swap");
 
+*,
+::before,
+::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  max-width: 620px;
+  margin: 0 auto;
+  padding-top: 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
 </style>
---
+++ src/components/UserCardSkeleton.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="user-card">
+        <div class="card-avatar">
+            <AnimatedPlaceholderVue
+                height="100px"
+                width="100px"
+                borderRadius="50%"
+            />
+        </div>
+        <div class="card-info">
+            <AnimatedPlaceholderVue
+                height="35px"
+                width="200px"
+            />
+            <div class="summary-placeholder">
+                <AnimatedPlaceholderVue
+                    height="16px"
+                />
+                <AnimatedPlaceholderVue
+                    height="16px"
+                />
+                <AnimatedPlaceholderVue
+                    height="16px"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import AnimatedPlaceholderVue from './AnimatedPlaceholder.vue';
+</script>
+
+<style scoped>
+.summary-placeholder {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    width: 100%;
+}
+
+</style>
